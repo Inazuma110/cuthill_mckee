8ea47fa602fde83e82b1518797ad171efe0a6092
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-04-22 16:14:56 +0900

    カットヒルマキー法を用いた行列を返す配列を作成

--- test.py
+++ test.py
@@ -1,25 +1,31 @@
 from scipy import sparse
 import numpy as np
 
-# m = np.array([[1, 7, 3], [7, 4, -5], [3, -5, 6]])
-m = np.array([[1, 1, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1, 1, 0, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    [0, 1, 0, 0, 1, 1, 0, 0, 0, 0],
-    [1, 0, 1, 0, 0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [0, 1, 1, 1, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0]])
-
+m = np.array([
+    [1, 0, 0, 0, 1, 0, 0, 0],
+    [0, 1, 1, 0, 0, 1, 0, 1],
+    [0, 1, 1, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 1, 0, 1],
+    [0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1, 0, 1]
+    ])
 print(m)
 print()
 
-m = sparse.csr_matrix(m)
-print(m)
-print()
+# 隣接行列
+def create_cuthill_mckee_matrix(adjacency_matrix):
+    csr_m = sparse.csr_matrix(adjacency_matrix)
+    index_dict = sparse.csgraph.reverse_cuthill_mckee(csr_m)
+    index_dict = {i[1]: i[0] for i in enumerate(index_dict)}
 
-res = sparse.csgraph.reverse_cuthill_mckee(m)
-print(res)
-print()
+    cuthill_mckee_matrix = np.zeros_like(adjacency_matrix)
+    row, column = csr_m.nonzero()
+
+    for r, c in zip(row, column):
+        cuthill_mckee_matrix[index_dict[r], index_dict[c]] = 1
+
+    return cuthill_mckee_matrix
+
+print(create_cuthill_mckee_matrix(m))
